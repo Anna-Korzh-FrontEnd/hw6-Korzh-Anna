dc1d9d3d4489ec1da25e02097078717b44490642
Anna-Korzh-FrontEnd <korzh_89@ukr.net>
2022-02-22 22:59:55 +0200

    Исправления

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 //1:
 
 // function sumTwoSmallestNumbers(...args){
-//     let sortedArray = args.sort((a, b) => a - b);
 //     if(sortedArray.length >1){
+//         let sortedArray = args.sort((a, b) => a - b);
 //         return sortedArray[0] + sortedArray[1];
 //     } else {
 //         console.error("Передано менее 2-х чисел") ;
 //     }
 // };
-//
+
 // console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
 // console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
 // console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
@@ -18,23 +18,23 @@
 //2:
 
 function createCalculator(number) {
+
     let object = {
-        x: number,
         sum: function (arg){
-            object.x += arg;
-            return object.x
+            number += arg;
+            return number;
         },
         mult: function (arg){
-            object.x = object.x * arg;
-            return object.x
+            number = number * arg;
+            return number
         },
         sub: function (arg) {
-            object.x = object.x - arg;
-            return object.x
+            number = number - arg;
+            return number
         },
         div: function (arg) {
-            object.x = object.x / arg;
-            return object.x
+            number = number / arg;
+            return number
         }
     }
     return object;
@@ -45,8 +45,8 @@ console.log(calc.sum(5)); /// 15
 console.log(calc.mult(10)); // 150
 console.log(calc.sub(40)); // 110
 console.log(calc.div(10)); // 11
-
-
+//
+//
 
 
 
